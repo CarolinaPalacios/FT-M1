0b3d76b317ae82d54c4fed9c422263e7b3fbcecf
Carolina Palacios <caaro.palacios@hotmail.com>
2023-05-08 16:30:44 -0300

    homework 1 resuelta

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,14 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let binario = num;
+   let decimal = parseInt(binario, 2);
+   return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+ return (num).toString(2);
+}
 
 module.exports = {
    BinarioADecimal,
